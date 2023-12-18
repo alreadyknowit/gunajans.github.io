2638f0c85dbb5fe8444376d36821411e9329c685
DESKTOP-PA7JC5C\Demir <demiriabdurrahman@gmail.com>
2023-12-18 18:52:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNavBar from './components/CustomNavBar';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './Routes'
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter basename='/gunjans.github.io'>
+      <CustomNavBar />
+      <AppRoutes />
+      </BrowserRouter>
+    
+    </div>
+  );
+}
+
+export default App;
